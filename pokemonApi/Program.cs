e52e7bae794dfa66ebfecdbb37cd2e056dcf9eb1
Axel Andersson <axel.andersson@elev.ga.ntig.se>
2025-05-24 18:20:03 +0200

    asd

--- pokemonApi/Program.cs
+++ pokemonApi/Program.cs
@@ -1,2 +1,11 @@
-﻿Console.WriteLine("hello world!");
+﻿HttpClient client = new();
+
+client.BaseAddress = new("https://pokeapi.co/api/v2/");
+
+HttpResponseMessage response = client.GetAsync("pokemon/snorlax").Result;
+
+string snorlax = response.Content.ReadAsStringAsync().Result;
+
+File.WriteAllText("snorlax.json", snorlax);
+
 Console.ReadLine();
